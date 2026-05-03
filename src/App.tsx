1ab0fffc2ab466313a7619d197822aadd5251b5d
Felix Zailskas <felixzailskas@gmail.com>
2026-05-03 17:31:10 +0200

    chore: scaffold Vite + React + TypeScript + Tailwind project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[var(--color-bg)] text-[var(--color-text)]">
+      <h1 className="text-2xl font-bold p-4">Fretlab</h1>
+    </div>
+  )
+}
+
+export default App
